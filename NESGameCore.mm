1e84bcf122d0589af673b81c04f119c092bc0dd0
C. Heath <hexleytheplatypus@icloud.com>
2024-04-19 08:24:59 -0400

    builds

--- NESGameCore.mm
+++ NESGameCore.mm
@@ -29,7 +29,6 @@
 #import <OpenEmuBase/OERingBuffer.h>
 #import "OENESSystemResponderClient.h"
 #import "OEFDSSystemResponderClient.h"
-#import <OpenGL/gl.h>
 
 #include <NstBase.hpp>
 #include <NstApiEmulator.hpp>
@@ -345,14 +344,14 @@ static __weak NESGameCore *_current;
     return OEIntSizeMake(_aspectWidth, _aspectHeight);
 }
 
-- (GLenum)pixelFormat
+- (uint32_t)pixelFormat
 {
-    return GL_BGRA;
+    return OEPixelFormat_BGRA;
 }
 
-- (GLenum)pixelType
+- (uint32_t)pixelType
 {
-    return GL_UNSIGNED_INT_8_8_8_8_REV;
+    return OEPixelType_UNSIGNED_INT_8_8_8_8_REV;
 }
 
 # pragma mark - Audio
